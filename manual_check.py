70ca4d4958d6ab478dedcc520bab6cef75d6445a
Youssra REBBOUD <100802809+RYoussra@users.noreply.github.com>
2023-02-06 18:25:26 +0100

    checking

--- manual_check.py
+++ manual_check.py
@@ -4,24 +4,38 @@ correct_ones=[]
 correct_trigger1=0
 correct_trigger2=0
 correct_Sentenecs=0
-for idx, row in enables.iterrows():
-    print(row['sentence'])
-    print(row['trigger1'])
-    print(row['trigger2'])
-    inp = input('is the sentence correct?')
-    if inp=='1':
-        correct_Sentenecs+=1
-        inp2 = input('is trigger1 correct?')
-        inp3 = input('is trigger2 correct?')
-        if inp2=='1':
-            correct_trigger1 += 1
-        if inp2=='1':
-            correct_trigger2 += 1
+
+for idx, row in enables.iloc[:2].iterrows():
+
+
+        print(row['sentence'])
+        print(row['trigger1'])
+        print(row['trigger2'])
+        inp = input('is the sentence correct?')
+        if inp=='1':
+            correct_Sentenecs+=1
+
+            inp2 = input('is trigger1 correct?')
+            inp3 = input('is trigger2 correct?')
+            if inp2=='1':
+                correct_trigger1 += 1
+                correct1=row['trigger1']
+
+            else:
+                correct1=input('correct please trigger1')
+
+            if inp3 == '1':
+                correct2=row['trigger2']
+                correct_trigger2 += 1
+            else:
+                correct2=input('correct please')
+            correct_ones.append([row['sentence'],correct1,correct2])
 
 
 
 
 
 
-    break
-
+print('correct_Sentenecs: ',correct_Sentenecs)
+print('correct_trigger1',correct_trigger1)
+print('correct_trigger2',correct_trigger2)
